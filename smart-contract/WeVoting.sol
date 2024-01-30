9e46d74658614a7f3ec64d0e5502d5789f862af5
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2024-01-29 22:14:39 -0300

    feat: multiple Candidate (with photoUri) current voting

--- smart-contract/WeVoting.sol
+++ smart-contract/WeVoting.sol
@@ -1,54 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-struct Voting {
-    string option1;
-    uint votes1;
-    string option2;
-    uint votes2;
-    uint maxDate;
+struct Candidate {
+    uint number;
+    string photoUri;
+    string name;
 }
 
 struct Vote {
-    uint choice; // 1 or 2
+    uint number;
     uint date;
 }
 
 contract WeVoting {
     address owner;
     uint public currentVoting;
-    Voting[] public votings;
+    uint public lastCandidateNumber;
+    uint public currentMaxDate;
+    mapping(uint => Candidate[]) public votings;
     mapping(uint => mapping(address => Vote)) public votes;
 
     constructor() {
         owner = msg.sender;
     }
 
-    function getCurrentVoting() public view returns(Voting memory) {
+    function getCurrentCandidates() public view returns(Candidate[] memory) {
         return votings[currentVoting];
     }
 
-    function addVoting(string memory option1, string memory option2, uint timeToVote) public {
+    function addVoting(uint timeToVote) public {
         require(msg.sender == owner, "Invalid sender");
 
-        if(votings.length > 0) ++currentVoting;
-
-        Voting memory newVoting;
-        newVoting.option1 = option1;
-        newVoting.option2 = option2;
-        newVoting.maxDate = timeToVote + block.timestamp;
-        votings.push(newVoting);
+        if(currentVoting > 0) ++currentVoting;
+        
+        currentMaxDate = timeToVote + block.timestamp;
+        lastCandidateNumber = 1;
     }
 
-    function addVote(uint choice) public {
-        require(choice == 1 || choice == 2, "Invalid choice");
-        require(getCurrentVoting().maxDate > block.timestamp, "No open votings");
-        require(votes[currentVoting][msg.sender].date == 0, "You already voted on this voting");
-        
-        votes[currentVoting][msg.sender].choice = choice;
-        votes[currentVoting][msg.sender].date = block.timestamp;
+    function addCandidate(string calldata name, string calldata photoUri) public {
+        Candidate memory newCandidate;
+        newCandidate.number = lastCandidateNumber++;
+        newCandidate.name = name;
+        newCandidate.photoUri = photoUri;
 
-        if (choice == 1) ++votings[currentVoting].votes1;
-        else ++votings[currentVoting].votes2;
+        votings[currentVoting].push(newCandidate);
+    }
+
+    function toVote(uint number) public {
+        require(number >= 1 && number <= votings[currentVoting].length, "No candidate with that number");
+        require(currentMaxDate > block.timestamp, "No open votings");
+        require(votes[currentVoting][msg.sender].date == 0, "You already voted");
+        
+        Vote memory vote;
+        vote.number = number;
+        vote.date = block.timestamp;
+        votes[currentVoting][msg.sender] = vote;
     }
 }
