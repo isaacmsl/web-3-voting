40265b78af454d3d7d47668d544d578a5912cba0
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2024-02-01 00:18:21 -0300

    feat: improve initial page style

--- dapp/src/app/page.tsx
+++ dapp/src/app/page.tsx
@@ -10,22 +10,22 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center gap-32 h-screen">
+    <div className="flex flex-col items-center justify-center sm:h-screen">
       <Head>
         <title>Web 3 Voting | Login</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <main className="flex flex-wrap gap-12 p-12 justify-center">
+      <main className="flex flex-col sm:flex-row gap-12 p-12 justify-center">
         <section className="flex flex-col items-start justify-center gap-12 md:gap-20">
           <header className="grid gap-4">
             <h1 className="text-4xl font-bold">Web 3 Voting</h1>
-            <p>Secure and privative votes using Blockchain tecnology.</p>
+            <p>An ilustrative secure and privative voting system using Blockchain tecnology.</p>
           </header>
           <button
             onClick={login}
             type="button"
-            className="bg-orange-500 rounded p-2 hover:scale-105 hover:font-bold flex gap-4 items-center"
+            className="bg-orange-700 rounded p-2 hover:scale-105 hover:font-bold flex gap-4 items-center"
           >
             <Image
               src="/MetaMask_Fox.svg"
@@ -36,7 +36,14 @@ export default function Home() {
             Connect with MetaMask
           </button>
         </section>
-        <iframe width={400} height={300} src="https://www.youtube.com/embed/EKzZOZUAbfg" title="Bringing Voting Systems into the Digital Age with Blockchain | Annika Jacobsen | TEDxZurichSalon"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <iframe
+          height={300}
+          className="border-2 border-orange-700 rounded hover:scale-105 cursor-pointer w-full"
+          src="https://www.youtube.com/embed/EKzZOZUAbfg"
+          title="Bringing Voting Systems into the Digital Age with Blockchain | Annika Jacobsen | TEDxZurichSalon"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        />
       </main>
       <footer className="flex flex-wrap-reverse justify-between p-12 gap-12 md:gap-40 text-gray-500">
         &copy; 2024 Web 3 Voting, Inc
